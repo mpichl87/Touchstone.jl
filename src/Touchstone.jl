2735b4730d826bc69d410b7f46eefea7adeebf1d
Michael Pichler <michael.pichler@meduniwien.ac.at>
2018-04-19 16:58:21 +0200

    NEW: parsing options and comments

--- src/Touchstone.jl
+++ src/Touchstone.jl
@@ -2,51 +2,146 @@ __precompile__()
 module Touchstone
 
 const units = Dict{ String, Float64 }(
-    "Hz"    =>      1e0,
-    "kHz"   =>      1e3,
-    "MHz"   =>      1e6,
-    "GHz"   =>      1e9
+    "HZ"    =>      1e0,
+    "KHZ"   =>      1e3,
+    "MHZ"   =>      1e6,
+    "GHZ"   =>      1e9
 )
-const default_units = "GHz"
+const default_units = "GHZ"
 
-const parameter = Dict{ String, String }(
-    "S" => "Scattering parameters",
-    "Y" => "Admittance parameters",
-    "Z" => "Impedance parameters",
-    "H" => "Hybrid-h parameters",
-    "G" => "Hybrid-g parameters"
+const parameters = Dict{ String, String }(
+    "S" => :ScatteringParameters,
+    "Y" => :AdmittanceParameters,
+    "Z" => :ImpedanceParameters,
+    "H" => :HybridHParameters,
+    "G" => :HybridGParameters,
 )
 const default_parameter = "S"
 
-const format = Dict{ String, String }(
-    "DB" => "decibel-angle",
-    "MA" => "magnitude-angle",
-    "RI" => "real-imaginary"
+const formats = Dict{ String, String }(
+    "DB" => :DecibelAngle,
+    "MA" => :MagnitudeAngle,
+    "RI" => :RealImaginary
 )
 const default_format = "MA"
 
 const default_resistance = 50.0
 
-export Options
 struct Options
     unit::Float64
-    parameter::String
-    format::String
+    parameter::Symbol
+    format::Symbol
     resistance::Float64
 end
+Options() = Options( units[ default_units ], parameters[ default_parameter ], formats[ default_format ], default_resistance )
+function equ( o1, o2 :: Options )
+    isapprox( o1.unit, o2.unit ) &&
+    o1.parameter == o2.parameter &&
+    o1.format == o2.format &&
+    isapprox( o1.resistance, o2.resistance )
+end
 
-export parse_option_line
-function parse_option_line( line::String = "" )
-    unit = units[ default_units ]
-    parameter = default_parameter
-    format = default_format
-    resistance = default_resistance
-    entries = split( line )
-    println( entries )
-    # for entry in entries
-    #
-    # end
+struct Comments
+    lines::Vector{String}
+end
+Comments() = Comments( [] )
+equ( c1, c2 :: Comments ) = c1.lines == c2.lines
+
+struct DataPoint
+    frequency::Float64
+    parameter::Matrix{Float64}
+end
+
+struct Data
+    points::Vector{DataPoint}
+end
+
+struct TS
+    options::Options
+    comments::Comments
+end
+TS() = TS( Options(), Comments() )
+function equ( ts1, ts2 :: TS )
+    equ( ts1.options, ts2.options ) &&
+    equ( ts1.comments, ts2.comments )
+end
+
+is_comment_line( line ) = length( line ) > 0 && line[ 1 ] == '!'
+
+parse_comment_line( line ) = line[ 2:end ]
+
+is_option_line( line ) = length( line ) > 0 && line[ 1 ] == '#'
+
+is_frequency_unit_option( option ) = haskey( units, option )
+
+parse_frequency_unit_option( option ) = units[ option ]
+
+is_parameter_option( option ) = haskey( parameters, option )
+
+parse_parameter_option( option ) = parameters[ option ]
+
+is_format_option( option ) = haskey( formats, option )
+
+parse_format_option( option ) = formats[ option ]
+
+is_resistance_option( option ) = option == "R"
+
+parse_resistance_option( option ) = parse( Float64, option )
+
+# Version 2.0 not implemented:
+is_keyword_line( line ) = length( line ) > 0 && line[ 1 ] == '['
+
+is_empy_line( line ) = length( line ) == 0
+
+function parse_option_line( line )
+    line = uppercase( line )
+
+    default = Options()
+    unit        = default.unit
+    parameter   = default.parameter
+    format      = default.format
+    resistance  = default.resistance
+
+    entries = split( line[ 2:end ] )
+
+    next_is_resistance = false
+    for entry in entries
+        if next_is_resistance
+            resistance = parse_resistance_option( entry )
+            next_is_resistance = false
+        else
+            if is_frequency_unit_option( entry )
+                unit = parse_frequency_unit_option( entry )
+            elseif is_parameter_option( entry )
+                parameter = parse_parameter_option( entry )
+            elseif is_format_option( entry )
+                format = parse_format_option( entry )
+            else
+                next_is_resistance =  is_resistance_option( entry )
+            end
+        end
+    end
     Options( unit, parameter, format, resistance )
 end
+
+function parse_touchstone_file( stream::IO )
+    options = Options()
+    first_option_line = true
+    comments = []
+    for line in eachline( stream )
+        if is_empy_line( line )
+            ;# nothing
+        elseif is_keyword_line( line )
+            throw( error( "Version 2.0 not implemented!" ) )
+        elseif is_comment_line( line )
+            push!( comments, parse_comment_line( line ) )
+        elseif first_option_line && is_option_line( line )
+            options = parse_option_line( line )
+            first_option_line = false
+        end
+    end
+    TS( options, Comments( comments ) )
+end
+parse_touchstone_file( in::String ) = parse_touchstone_file( IOBuffer( in ) )
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,119 @@
 import Touchstone
-
+TS = Touchstone
 using Base.Test
 
-# @test Touchstone.parse_option_line() == Touchstone.Options( 1e9, "S", "MA", 50.0 )
-println( Touchstone.parse_option_line() )
-println( Touchstone.Options( 1e9, "S", "MA", 50.0 ) )
+# @test ! TS.is_comment_line( "" )
+# @test   TS.is_comment_line( "!" )
+# @test   TS.is_comment_line( "!comment" )
+#
+# @test   TS.parse_comment_line( "!" ) == ""
+# @test   TS.parse_comment_line( "!comment" ) == "comment"
+#
+# @test ! TS.is_option_line( "" )
+# @test   TS.is_option_line( "#" )
+# @test   TS.is_option_line( "#options" )
+#
+# @test ! TS.is_frequency_unit_option( "" )
+# @test   TS.is_frequency_unit_option( "Hz" )
+# @test   TS.is_frequency_unit_option( "kHz" )
+# @test   TS.is_frequency_unit_option( "MHz" )
+# @test   TS.is_frequency_unit_option( "GHz" )
+# @test   TS.is_frequency_unit_option( "hz" )
+# @test   TS.is_frequency_unit_option( "khz" )
+# @test   TS.is_frequency_unit_option( "mhz" )
+# @test   TS.is_frequency_unit_option( "ghz" )
+# @test ! TS.is_frequency_unit_option( "uHz" )
+#
+# @test isapprox( TS.parse_frequency_unit_option( "Hz" ), 1e0 )
+# @test isapprox( TS.parse_frequency_unit_option( "kHz" ), 1e3 )
+# @test isapprox( TS.parse_frequency_unit_option( "MHz" ), 1e6 )
+# @test isapprox( TS.parse_frequency_unit_option( "GHz" ), 1e9 )
+# @test isapprox( TS.parse_frequency_unit_option( "hz" ), 1e0 )
+# @test isapprox( TS.parse_frequency_unit_option( "khz" ), 1e3 )
+# @test isapprox( TS.parse_frequency_unit_option( "mhz" ), 1e6 )
+# @test isapprox( TS.parse_frequency_unit_option( "ghz" ), 1e9 )
+#
+# @test ! TS.is_parameter_option( "" )
+# @test   TS.is_parameter_option( "S" )
+# @test   TS.is_parameter_option( "Y" )
+# @test   TS.is_parameter_option( "Z" )
+# @test   TS.is_parameter_option( "H" )
+# @test   TS.is_parameter_option( "G" )
+# @test   TS.is_parameter_option( "s" )
+# @test   TS.is_parameter_option( "y" )
+# @test   TS.is_parameter_option( "z" )
+# @test   TS.is_parameter_option( "h" )
+# @test   TS.is_parameter_option( "g" )
+# @test ! TS.is_parameter_option( "T" )
+#
+# @test ! TS.is_format_option( "" )
+# @test   TS.is_format_option( "DB" )
+# @test   TS.is_format_option( "MA" )
+# @test   TS.is_format_option( "RI" )
+# @test   TS.is_format_option( "db" )
+# @test   TS.is_format_option( "ma" )
+# @test   TS.is_format_option( "ri" )
+# @test ! TS.is_format_option( "BLA" )
 
-Touchstone.parse_option_line() == Touchstone.Options( 1e9, "S", "MA", 50.0 ) 
+@test   TS.equ( TS.parse_option_line( "#" ),              TS.Options( 1e9, :ScatteringParameters, :MagnitudeAngle,    50.0 ) )
+@test   TS.equ( TS.parse_option_line( "#kHz" ),           TS.Options( 1e3, :ScatteringParameters, :MagnitudeAngle,    50.0 ) )
+@test   TS.equ( TS.parse_option_line( "# kHz" ),          TS.Options( 1e3, :ScatteringParameters, :MagnitudeAngle,    50.0 ) )
+@test   TS.equ( TS.parse_option_line( "#MHz Y" ),         TS.Options( 1e6, :AdmittanceParameters, :MagnitudeAngle,    50.0 ) )
+@test   TS.equ( TS.parse_option_line( "# GHz Z dB" ),     TS.Options( 1e9, :ImpedanceParameters,  :DecibelAngle,      50.0 ) )
+@test   TS.equ( TS.parse_option_line( "#Hz H RI R 1" ),   TS.Options( 1.0, :HybridHParameters,    :RealImaginary,      1.0 ) )
+@test   TS.equ( TS.parse_option_line( "# Hz G RI R 50" ), TS.Options( 1.0, :HybridGParameters,    :RealImaginary,     50.0 ) )
+
+@test   TS.equ( TS.Comments( [ "a" ] ), TS.Comments( [ "a" ] ) )
+@test   TS.equ( TS.Comments( [ "a", "b" ] ), TS.Comments( [ "a", "b" ] ) )
+@test ! TS.equ( TS.Comments( [ "b", "a" ] ), TS.Comments( [ "a", "b" ] ) )
+@test ! TS.equ( TS.Comments( [ "a" ] ), TS.Comments( [ "a", "b" ] ) )
+
+@test   TS.equ( TS.Comments(), TS.Comments( [] ) )
+@test   TS.equ( TS.Options(), TS.Options( 1e9, :ScatteringParameters, :MagnitudeAngle, 50.0 ) )
+@test   TS.equ( TS.TS( TS.Options( 1e9, :ScatteringParameters, :MagnitudeAngle, 50.0 ), TS.Comments( [] ) ), TS.TS() )
+
+@test   TS.equ( TS.parse_touchstone_file( "" ), TS.TS() )
+@test   TS.equ( TS.parse_touchstone_file( """
+    !Test
+    """ ), TS.TS( TS.Options(), TS.Comments( [ "Test" ] ) ) )
+@test   TS.equ( TS.parse_touchstone_file( """
+        !Test1
+
+        # Hz G RI R 50
+        !Test2
+        """ ), TS.TS(   TS.Options( 1.0, :HybridGParameters, :RealImaginary, 50.0 ),
+                        TS.Comments( [ "Test1", "Test2" ] ) ) )
+@test   TS.equ( TS.parse_touchstone_file( """
+        # Hz G RI R 50
+
+        !Test1
+        # GHz Z dB
+        !Test2
+        """ ), TS.TS(   TS.Options( 1.0, :HybridGParameters, :RealImaginary, 50.0 ),
+                        TS.Comments( [ "Test1", "Test2" ] ) ) )
+@test   TS.equ( TS.parse_touchstone_file( """
+        # GHz Z dB
+        # Hz G RI R 50
+        !Test1
+
+        !Test2
+        """ ), TS.TS(   TS.Options( 1e9, :ImpedanceParameters, :DecibelAngle, 50.0 ),
+                        TS.Comments( [ "Test1", "Test2" ] ) ) )
+
+
+
+TS.Data( [ TS.DataPoint( 1.0, [ 1 2; 3 4]) ] )
+
+
+
+
+
+
+
+@test_throws MethodError TS.parse_touchstone_file( """
+        # GHz Z dB
+        # Hz G RI R 50
+        !Test1
+        !Test2
+        [test]
+        """ )
